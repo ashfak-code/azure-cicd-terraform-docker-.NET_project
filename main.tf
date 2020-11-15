dfe080634ff2e5b8acbc744329ca0e3da9e43306
unknown <mailtoashfak@gmail.com>
2020-11-15 16:32:03 +0530

    tf file uploaded

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "azurerm" {
+    version = "2.5.0"
+    features {}
+}
+
+resource "azurerm_resource_group" "tf_test" {
+  name = "tfmainrg"
+  location = "eastus2"
+}
+
+resource "azurerm_container_group" "tfcg_test" {
+  name                      = "weatherapi"
+  location                  = azurerm_resource_group.tf_test.location
+  resource_group_name       = azurerm_resource_group.tf_test.name
+
+  ip_address_type     = "public"
+  dns_name_label      = "ashfakcode"
+  os_type             = "Linux"
+
+  container {
+      name            = "weatherapi"
+      image           = "ashfakcode/weatherapi"
+        cpu             = "1"
+        memory          = "1"
+
+        ports {
+            port        = 80
+            protocol    = "TCP"
+        }
+  }
+}
